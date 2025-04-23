33b658f007a18e8ad8c22d6064ccef8bdfb9947d
LeRueHein <martin.trely@iia-formation.fr>
2025-04-23 16:50:42 +0200

    ajout test failed

---
+++ src/test/AltoroJLoginFail_Test_A1F020413181AB30EBD4B53939373936.java
@@ -0,0 +1,92 @@
+/**@generated
+ * WARNING � Changes you make to this file may be lost.
+ *           File is generated and may be re-generated without warning.
+ * @RPT-Core-generated Version 8.6
+ */
+/******************************************************************
+* Licensed Materials - Property of IBM and/or HCL
+* Copyright IBM Corporation 2013, 2017. All Rights Reserved.
+* Copyright HCL Technologies Ltd. 2017, 2020.  All Rights Reserved.
+* 
+* U.S. Government Users Restricted Rights - Use, duplication or 
+* disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
+******************************************************************/
+/* TestScript.template - Suitable for collaborative code generation */
+package test;
+
+import com.ibm.rational.test.lt.datacorrelation.execution.proto.ProtoAdapterHandler;
+import com.ibm.rational.test.lt.execution.core.impl.LTTestScript;
+import com.ibm.rational.test.lt.execution.moeb.action.EndMoebStepBatchAction;
+import com.ibm.rational.test.lt.execution.moeb.services.MoebActionFailedEvent;
+import com.ibm.rational.test.lt.execution.moeb.services.MoebFatalErrorEvent;
+import com.ibm.rational.test.lt.kernel.action.IContainer;
+import com.ibm.rational.test.lt.kernel.action.impl.KThrow;
+import com.ibm.rational.test.lt.kernel.services.*;
+import com.ibm.rational.test.lt.kernel.services.RPTEventStructure;
+
+@SuppressWarnings("all")
+public class AltoroJLoginFail_Test_A1F020413181AB30EBD4B53939373936 extends LTTestScript  {
+
+    static ProtoAdapterHandler pa = new ProtoAdapterHandler();
+    static {           
+        pa.addPA("com.ibm.rational.test.lt.datacorrelation.execution.protocol.core.CoreProtoAdapter", "coreAdapter");
+pa.addPA("com.ibm.rational.test.lt.execution.moeb.dc.MoebDataAdapter", "com.ibm.rational.test.lt.execution.moeb.action.MoebStepBatchAction");
+    }
+	
+	
+	
+    
+    
+	
+	public AltoroJLoginFail_Test_A1F020413181AB30EBD4B53939373936(IContainer container, String invocationId) {
+		super(container, "AltoroJLoginFail", invocationId, "A1F020413181AB30EBD4B53939373936", "/DemoAltoroJTest/Tests/AltoroJLoginFail.testsuite", "/home/devsecop_demo/IBM/devopstest/workspace/DemoAltoroJTest/Tests/AltoroJLoginFail.testsuite");
+		setArmEnabled(false);
+		stopAtYourConvenience=false;
+		
+	}
+
+    public void execute() {
+    	try {
+			
+	    		if (!isScheduleRun()) setThinkMax(2000);
+	
+	if (!isScheduleRun() && !isCompoundTestRun()){
+this.addEventBehavior(new RPTEventStructure(new RPTFailVPEvent(), new RPTContinueEvent("Content Verification Point failed"), 1, "Content Verification Point failed"));
+	this.addEventBehavior(new RPTEventStructure(new RPTConnectEvent(), new RPTContinueEvent("Connection failed"), 1, "Connection failed"));
+	this.addEventBehavior(new RPTEventStructure(new RPTAuthenticationEvent(), new RPTContinueEvent("Authentication failed"), 1, "Authentication failed"));
+	this.addEventBehavior(new RPTEventStructure(new RPTDataPoolEOFEvent(), new RPTStopUserEvent("End of dataset reached"), 1, "End of dataset reached"));
+	this.addEventBehavior(new RPTEventStructure(new RPTTestVerdictFailedEvent(), new RPTContinueEvent("Test verdict was set to fail"), 2, "Test verdict was set to fail"));
+	this.addEventBehavior(new RPTEventStructure(new RPTReferenceEvent(), new RPTContinueEvent("Failed to extract reference"), 1, "Failed to extract reference"));
+	this.addEventBehavior(new RPTEventStructure(new RPTSubstitutionEvent(), new RPTContinueEvent("Substitution failed"), 1, "Substitution failed"));
+	this.addEventBehavior(new RPTEventStructure(new RPTServerTimeoutEvent(), new RPTContinueEvent("Timeout"), 1, "Timeout"));
+	this.addEventBehavior(new RPTEventStructure(new RPTDroppedConnectionEvent(), new RPTContinueEvent("Server dropped connection"), 1, "Server dropped connection"));
+	this.addEventBehavior(new RPTEventStructure(new RPTCustomCodeVPFailureEvent(), new RPTContinueEvent("Custom Verification Point failed"), 1, "Custom Verification Point failed"));
+	this.addEventBehavior(new RPTEventStructure(new RPTCustomCodeAlertEvent(), new RPTContinueEvent("Custom Code reported an Alert"), 0, "Custom Code reported an Alert"));
+	this.addEventBehavior(new RPTEventStructure(new RPTCustomCodeExceptionEvent(), new RPTStopUserEvent("Custom Code reported an unhandled exception"), 1, "Custom Code reported an unhandled exception"));
+	this.addEventBehavior(new RPTEventStructure(new MoebActionFailedEvent(), new RPTContinueEvent("Playback of UI Action failed"), 0, "Playback of UI Action failed"));
+	this.addEventBehavior(new RPTEventStructure(new MoebFatalErrorEvent(), new RPTStopUserEvent("Playback of UI step has a fatal error"), 0, "Playback of UI step has a fatal error"));
+	}
+	
+	
+	        
+			this.addFinally(new EndMoebStepBatchAction(this));
+
+	    } catch (Throwable e) {
+		     log("Test Execution: AltoroJLoginFail_Test_A1F020413181AB30EBD4B53939373936 ",e);
+	    } finally {
+	        super.execute();
+		}	    
+    }
+    
+	public void preFinish() {
+		
+		super.preFinish();
+	}
+	
+	public void stop() {
+		
+		super.stop();
+	}	
+	
+	
+}
---
+++ src/test/Demo2_Test_A1F0204245571270EBD4B53939373936.java
@@ -0,0 +1,177 @@
+/**@generated
+ * WARNING � Changes you make to this file may be lost.
+ *           File is generated and may be re-generated without warning.
+ * @RPT-Core-generated Version 8.6
+ */
+/******************************************************************
+* Licensed Materials - Property of IBM and/or HCL
+* Copyright IBM Corporation 2013, 2017. All Rights Reserved.
+* Copyright HCL Technologies Ltd. 2017, 2020.  All Rights Reserved.
+* 
+* U.S. Government Users Restricted Rights - Use, duplication or 
+* disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
+******************************************************************/
+/* TestScript.template - Suitable for collaborative code generation */
+package test;
+
+import com.ibm.rational.test.lt.datacorrelation.execution.proto.ProtoAdapterHandler;
+import com.ibm.rational.test.lt.datacorrelation.execution.sub.DataSub;
+import com.ibm.rational.test.lt.datacorrelation.execution.sub.IDataSub;
+import com.ibm.rational.test.lt.datacorrelation.execution.sub.ISubRule;
+import com.ibm.rational.test.lt.datacorrelation.execution.sub.SubRule;
+import com.ibm.rational.test.lt.execution.core.impl.ArmableTransaction;
+import com.ibm.rational.test.lt.execution.core.impl.LTTestScript;
+import com.ibm.rational.test.lt.execution.moeb.action.EndMoebStepBatchAction;
+import com.ibm.rational.test.lt.execution.moeb.services.MoebActionFailedEvent;
+import com.ibm.rational.test.lt.execution.moeb.services.MoebFatalErrorEvent;
+import com.ibm.rational.test.lt.kernel.action.IContainer;
+import com.ibm.rational.test.lt.kernel.action.IKAction;
+import com.ibm.rational.test.lt.kernel.action.impl.KThrow;
+import com.ibm.rational.test.lt.kernel.services.*;
+import com.ibm.rational.test.lt.kernel.services.RPTEventStructure;
+
+@SuppressWarnings("all")
+public class Demo2_Test_A1F0204245571270EBD4B53939373936 extends LTTestScript  {
+
+    static ProtoAdapterHandler pa = new ProtoAdapterHandler();
+    static {           
+        pa.addPA("com.ibm.rational.test.lt.datacorrelation.execution.protocol.core.CoreProtoAdapter", "coreAdapter");
+pa.addPA("com.ibm.rational.test.lt.execution.moeb.dc.MoebDataAdapter", "com.ibm.rational.test.lt.execution.moeb.action.MoebStepBatchAction");
+    }
+	
+	
+	
+    
+    
+	
+	public Demo2_Test_A1F0204245571270EBD4B53939373936(IContainer container, String invocationId) {
+		super(container, "demo2", invocationId, "A1F0204245571270EBD4B53939373936", "/DemoAltoroJTest/demo2.testsuite", "/home/devsecop_demo/IBM/devopstest/workspace/DemoAltoroJTest/demo2.testsuite");
+		setArmEnabled(false);
+		stopAtYourConvenience=false;
+		
+	}
+
+    public void execute() {
+    	try {
+			
+	    		if (!isScheduleRun()) setThinkMax(2000);
+	
+	if (!isScheduleRun() && !isCompoundTestRun()){
+this.addEventBehavior(new RPTEventStructure(new RPTFailVPEvent(), new RPTContinueEvent("Content Verification Point failed"), 1, "Content Verification Point failed"));
+	this.addEventBehavior(new RPTEventStructure(new RPTConnectEvent(), new RPTContinueEvent("Connection failed"), 1, "Connection failed"));
+	this.addEventBehavior(new RPTEventStructure(new RPTAuthenticationEvent(), new RPTContinueEvent("Authentication failed"), 1, "Authentication failed"));
+	this.addEventBehavior(new RPTEventStructure(new RPTDataPoolEOFEvent(), new RPTStopUserEvent("End of dataset reached"), 1, "End of dataset reached"));
+	this.addEventBehavior(new RPTEventStructure(new RPTTestVerdictFailedEvent(), new RPTContinueEvent("Test verdict was set to fail"), 2, "Test verdict was set to fail"));
+	this.addEventBehavior(new RPTEventStructure(new RPTReferenceEvent(), new RPTContinueEvent("Failed to extract reference"), 1, "Failed to extract reference"));
+	this.addEventBehavior(new RPTEventStructure(new RPTSubstitutionEvent(), new RPTContinueEvent("Substitution failed"), 1, "Substitution failed"));
+	this.addEventBehavior(new RPTEventStructure(new RPTServerTimeoutEvent(), new RPTContinueEvent("Timeout"), 1, "Timeout"));
+	this.addEventBehavior(new RPTEventStructure(new RPTDroppedConnectionEvent(), new RPTContinueEvent("Server dropped connection"), 1, "Server dropped connection"));
+	this.addEventBehavior(new RPTEventStructure(new RPTCustomCodeVPFailureEvent(), new RPTContinueEvent("Custom Verification Point failed"), 1, "Custom Verification Point failed"));
+	this.addEventBehavior(new RPTEventStructure(new RPTCustomCodeAlertEvent(), new RPTContinueEvent("Custom Code reported an Alert"), 0, "Custom Code reported an Alert"));
+	this.addEventBehavior(new RPTEventStructure(new RPTCustomCodeExceptionEvent(), new RPTStopUserEvent("Custom Code reported an unhandled exception"), 1, "Custom Code reported an unhandled exception"));
+	this.addEventBehavior(new RPTEventStructure(new MoebActionFailedEvent(), new RPTContinueEvent("Playback of UI Action failed"), 0, "Playback of UI Action failed"));
+	this.addEventBehavior(new RPTEventStructure(new MoebFatalErrorEvent(), new RPTStopUserEvent("Playback of UI step has a fatal error"), 0, "Playback of UI step has a fatal error"));
+	}
+	
+	
+	        	this.add(transaction_1(this));
+			this.addFinally(new EndMoebStepBatchAction(this));
+
+	    } catch (Throwable e) {
+		     log("Test Execution: Demo2_Test_A1F0204245571270EBD4B53939373936 ",e);
+	    } finally {
+	        super.execute();
+		}	    
+    }
+    
+	public void preFinish() {
+		
+		super.preFinish();
+	}
+	
+	public void stop() {
+		
+		super.stop();
+	}	
+	
+	
+	private ArmableTransaction transaction_1(final IContainer parent) {
+		ArmableTransaction trx = new ArmableTransaction(parent, "demo2", "A1F020424557D5C0EBD4B53939373936") {
+		
+		    public void execute() {     
+		       	
+	
+	
+	
+	      
+                	this.add(transaction_2(this));
+
+		     
+                super.execute();
+            }
+        
+            public void finish(IKAction child) {
+                super.finish(child);
+                if (hasFinished()) {
+                    
+                }
+            }
+		};
+		trx.setArmEnabled(false);
+		return trx;
+	}
+
+
+	private ArmableTransaction transaction_2(final IContainer parent) {
+		ArmableTransaction trx = new ArmableTransaction(parent, "demo2.1", "A1F020424557D5C3EBD4B53939373936") {
+		
+		    public void execute() {     
+		       	
+	
+	
+	
+	      
+                	this.add(transaction_3(this));
+
+		     
+                super.execute();
+            }
+        
+            public void finish(IKAction child) {
+                super.finish(child);
+                if (hasFinished()) {
+                    
+                }
+            }
+		};
+		trx.setArmEnabled(false);
+		return trx;
+	}
+
+
+	private ArmableTransaction transaction_3(final IContainer parent) {
+		ArmableTransaction trx = new ArmableTransaction(parent, "demo2.1.1", "A1F020424557FCD0EBD4B53939373936") {
+		
+		    public void execute() {     
+		       	
+	
+	
+	
+	      
+                
+		     
+                super.execute();
+            }
+        
+            public void finish(IKAction child) {
+                super.finish(child);
+                if (hasFinished()) {
+                    
+                }
+            }
+		};
+		trx.setArmEnabled(false);
+		return trx;
+	}
+
+}
